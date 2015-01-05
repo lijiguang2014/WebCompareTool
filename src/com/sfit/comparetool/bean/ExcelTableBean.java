cd2da04391b5529ed6996437bc6cdb9c0ed4e301
lijiguang2014 <jiguang.li1112@gmail.com>
2015-01-05 17:32:30 +0800

    下班同步

---
+++ src/com/sfit/comparetool/bean/ExcelTableBean.java
@@ -0,0 +1,31 @@
+package com.sfit.comparetool.bean;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExcelTableBean {
+	
+	private String tablename;
+	private EntityBean entity;
+	private Map<String, DefineIndex> indexes = new HashMap<String, DefineIndex>();
+	
+	public String getTablename() {
+		return tablename;
+	}
+	public void setTablename(String tablename) {
+		this.tablename = tablename;
+	}
+	public EntityBean getEntity() {
+		return entity;
+	}
+	public void setEntity(EntityBean entity) {
+		this.entity = entity;
+	}
+	public Map<String, DefineIndex> getIndexes() {
+		return indexes;
+	}
+	public void setIndexes(Map<String, DefineIndex> indexes) {
+		this.indexes = indexes;
+	}
+	
+}
