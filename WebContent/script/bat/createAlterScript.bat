57c49952768aa1cb3004449c2596b2b322f2b0ab
lijiguang2014 <jiguang.li1112@gmail.com>
2015-01-13 09:32:06 +0800

    修复bug
    修复调用批处理createAlterScript.bat发生的问题

--- WebContent/script/bat/createAlterScript.bat
+++ WebContent/script/bat/createAlterScript.bat
@@ -2,5 +2,6 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
 set path=%path%;D:\program\pump\bin
 set include=%include%;D:\program\pump\include
 set lib=%lib%;D:\program\pump\lib
-pump %~dp0..\..\%1 %~dp0..\..\%2 %~dp0..\..\%3
+cd /d "%~dp0..\..\"
+pump "%1" "%2" "%3"
 rem pump D:\Working\GeneraterAlter\alter.sql D:\Working\GeneraterAlter\alter.sql.tpl D:\Working\GeneraterAlter\compareResult.xml
